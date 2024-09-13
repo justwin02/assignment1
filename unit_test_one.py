c8538bfd8b8fd690146f7de2388d740a090ba9ec
justwin02 <146994114+justwin02@users.noreply.github.com>
2024-09-13 11:17:11 -0500

    Delete unit_test_one.py

--- unit_test_one.py
+++
@@ -1,9 +0,0 @@
-import program
-import unittest
-
-class TestSum(unittest.TestCase):
-    def test_integer(self):
-        self.assertEqual(program.multiply(3, 3), 9)
-
-if __name__ == '__main__':
-    unittest.main()
